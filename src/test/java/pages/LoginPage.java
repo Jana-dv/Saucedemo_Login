5e8773f1fe0123c8f7f7cfa619a07cec4204fe57
Jana-dv <yana.devrisheva@gmail.com>
2025-09-22 18:46:10 +0300

    Fixed BaseTest, LoginPage, .gitignore.

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -4,16 +4,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
-WebDriver browser;
-    public LoginPage(WebDriver browser) {
-        this.browser = browser;
-    }
+
     private static final By USERNAME_INPUT = By.xpath("//*[@placeholder='Username']");
     private static final By PASSWORD_INPUT = By.xpath("//*[@placeholder='Password']");
     private static final By LOGIN_BUTTON = By.cssSelector(".submit-button");
+
+    WebDriver browser;
+
+    public LoginPage(WebDriver browser) {
+        this.browser = browser;
+    }
+
     public void open() {
         browser.get("https://www.saucedemo.com/");
     }
+
     public void login(String username, String password) {
         browser.findElement(USERNAME_INPUT).sendKeys(username);
         browser.findElement(PASSWORD_INPUT).sendKeys(password);
