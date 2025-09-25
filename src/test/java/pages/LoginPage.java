91064eb512920e268c1dbe6517ffbb95904df111
Jana-dv <yana.devrisheva@gmail.com>
2025-09-25 23:56:56 +0300

    Added DataProvider and AddGoodsToCartTest.

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -16,14 +16,26 @@ public class LoginPage extends BasePage {
 
     public void open() {
         driver.get(BASE_URL + "inventory.html");
-
     }
 
     public void login(String username, String password) {
+        fillLoginField(username);
+        fillPasswordField(password);
+        clickSubmit();
+    }
+
+    public void fillLoginField(String username) {
         driver.findElement(USERNAME_INPUT).sendKeys(username);
+    }
+
+    public void fillPasswordField(String password) {
         driver.findElement(PASSWORD_INPUT).sendKeys(password);
+    }
+
+    public void clickSubmit() {
         driver.findElement(LOGIN_BUTTON).click();
     }
+
 
     public String checkErrorMessage() {
         return driver.findElement(ERROR).getText();
